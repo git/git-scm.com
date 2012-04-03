8839ec5eedc8cf1790b77cf7c04029c69eab7e0d
Scott Chacon <schacon@gmail.com>
2012-04-03 10:11:21 -0700

    basic search functionality

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,3 +1,7 @@
+# t.text :blob_sha
+# t.text :plain
+# t.text :html
+# t.timestamps
 class Doc < ActiveRecord::Base
   has_many :doc_versions
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,16 @@ Gitscm::Application.routes.draw do
   match "/doc/videos" => "doc#videos"
   match "/doc/ext" => "doc#ext"
 
+  match "/search" => "site#search"
+
+  # mapping for jasons mocks
+  match "/documentation" => "doc#index"
+  match "/documentation/reference" => "doc#ref"
+  match "/documentation/reference/:file.html" => "doc#man"
+  match "/documentation/book" => "doc#book"
+  match "/documentation/videos" => "doc#videos"
+  match "/documentation/external-links" => "doc#ext"
+
   # TODO: old routes to new pages
 
   root :to => 'site#index'
