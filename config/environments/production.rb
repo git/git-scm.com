2ac8adb0b921b4e54357b5491a69e9801877d319
Scott Chacon <schacon@gmail.com>
2012-04-04 06:48:12 -0700

    finally?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Gitscm::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( git-scm.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
