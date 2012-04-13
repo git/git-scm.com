539a99fed20c54c2cb6e91c46f7a7bc001a7dbec
Scott Chacon <schacon@gmail.com>
2012-04-13 15:44:46 -0700

    random tagline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     render part
   end
 
+  def random_tagline
+    tags = ['fast-version-control',
+     'everything-is-local',
+     'distributed-even-if-your-workflow-isnt',
+     'local-branching-on-the-cheap',
+     'not-your-daddys-version-control',
+     'distributed-is-the-new-centralized' ]
+    mtag = tags[rand(tags.length)]
+    "<em>--</em>#{mtag}"
+  end
+
 end
