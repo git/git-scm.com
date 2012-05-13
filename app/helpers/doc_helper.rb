27a557cd24c9be3cb0eada353dc080ea2353606d
bry4n <bryann83@gmail.com>
2012-05-13 16:25:38 -0500

    refactor helpers to use content_tag/image_tag/link_to

--- app/helpers/doc_helper.rb
+++ app/helpers/doc_helper.rb
@@ -1,10 +1,13 @@
 module DocHelper
+  
   def man(name)
-    "<a href=\"/docs/#{name}\">#{name.gsub('git-', '')}</a>".html_safe
+    link_to name.gsub('git-', ''), doc_file_path(name)
   end
+
   def linkify(content, section)
     next_page = section.next_slug
     prev_page = section.prev_slug
     content.gsub('[[nav-next]]', next_page).gsub('[[nav-prev]]', prev_page)
   end
+
 end
