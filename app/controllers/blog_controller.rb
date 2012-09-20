25563fcdd1fcd82dc0d83cec3c581baf2b6125b4
bry4n <bryann83@gmail.com>
2012-09-20 17:49:56 -0500

    refactor blog controller & add blog presenter

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -6,21 +6,14 @@ class BlogController < ApplicationController
     month = params[:month]
     day   = params[:day]
     slug  = params[:slug]
-    file  = "#{year}-#{month}-#{day}-#{slug}"
-    @path = "#{Rails.root}/app/views/blog/progit/#{file}"
-    content = ''
-    if File.exists?("#{@path}.markdown")
-      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-      content = File.read("#{@path}.markdown")
-      content, @frontmatter = extract_frontmatter(content)
-      @content = markdown.render(content)
-    elsif File.exists?("#{path}.html")
-      content = File.read("#{path}.html")
-      @content, @frontmatter = extract_frontmatter(content)
+    file  = [year, month, day, slug] * "-"
+    blog = BlogPresenter.new(file)
+    if blog.exists?
+      @content, @frontmatter = blog.render
     else
       raise PageNotFound
     end
-
+    render :progit
   end
 
   # for Gitscm blog
@@ -30,14 +23,5 @@ class BlogController < ApplicationController
 
   private
 
-  def extract_frontmatter(content)
-    if content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)(.*)/m
-      cnt = $3
-      data = YAML.load($1)
-      [cnt, data]
-    else
-      [content, {}]
-    end
-  end
-
+ 
 end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Gitscm
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/app/presenters)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
