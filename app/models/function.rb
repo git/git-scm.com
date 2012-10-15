86016cf7e983262816d6c53925600928a0285068
Vicent Marti <tanoku@gmail.com>
2012-10-15 22:19:53 +0200

    Add Function#group_name

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,6 +1,10 @@
 class Function
   include MongoDoc
 
+  def group_name
+    @doc['group']
+  end
+
   def group
     @group ||= Group.new(
       Function.collection.find('_id' => @doc['group'], 'version' => version))
