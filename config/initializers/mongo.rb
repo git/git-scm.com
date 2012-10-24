4d0d5e4655a161227dd3cf67f883c61830140b39
Vicent Marti <tanoku@gmail.com>
2012-10-24 19:39:20 +0200

    Proper Mongo init

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,1 +1,9 @@
-Rails.configuration.mongo_db = Mongo::Connection.new["gitscm-library-#{Rails.env}"]
+if Rails.env == 'production'
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  Rails.configuration.mongo_db = db_connection
+else
+  Rails.configuration.mongo_db = Mongo::Connection.new["gitscm-library-#{Rails.env}"]
+end
