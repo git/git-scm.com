06b768623697c92ceb3bf2fe392b434e8622e293
Vicent Marti <tanoku@gmail.com>
2012-10-24 20:10:09 +0200

    Switch to MongoLab

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,9 +1,11 @@
+require 'uri'
+require 'mongo'
+
 if Rails.env == 'production'
-  db = URI.parse(ENV['MONGOHQ_URL'])
-  db_name = db.path.gsub(/^\//, '')
-  db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-  db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  Rails.configuration.mongo_db = db_connection
+  uri  = URI.parse(ENV['MONGOLAB_URI'])
+  conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  db   = conn.db(uri.path.gsub(/^\//, ''))
+  Rails.configuration.mongo_db = db
 else
   Rails.configuration.mongo_db = Mongo::Connection.new["gitscm-library-#{Rails.env}"]
 end
