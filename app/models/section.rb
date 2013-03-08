01db95e75f9352ac49c739204a83564a8cce35dc
Jeff King <peff@peff.net>
2013-03-08 05:31:45 -0500

    url-encode book section titles
    Some of the translated section titles end in question marks.
    When these are placed directly into a link without quoting,
    the trailing "?" is parsed as a parameter separator, leading
    to a broken link.
    
    The same problem was fixed in #149, but only for the actual
    section listing. This commit fixes the prev/next links, as
    well as any direct chapter links.

--- app/models/section.rb
+++ app/models/section.rb
@@ -37,12 +37,12 @@ class Section < ActiveRecord::Base
     lang = self.book.code
     prev_number = self.number - 1
     if section = self.sections.where(:number => prev_number).first
-      return "/book/#{lang}/#{section.slug}"
+      return "/book/#{lang}/#{ERB::Util.url_encode(section.slug)}"
     else
       # find previous chapter
       if ch = self.chapter.prev
         if section = ch.last_section
-          return "/book/#{lang}/#{section.slug}"
+          return "/book/#{lang}/#{ERB::Util.url_encode(section.slug)}"
         end
       end
     end
@@ -53,11 +53,11 @@ class Section < ActiveRecord::Base
     lang = self.book.code
     next_number = self.number + 1
     if section = self.sections.where(:number => next_number).first
-      return "/book/#{lang}/#{section.slug}"
+      return "/book/#{lang}/#{ERB::Util.url_encode(section.slug)}"
     else
       if ch = self.chapter.next
         if section = ch.first_section
-          return "/book/#{lang}/#{section.slug}"
+          return "/book/#{lang}/#{ERB::Util.url_encode(section.slug)}"
         end
       end
       # find next chapter
