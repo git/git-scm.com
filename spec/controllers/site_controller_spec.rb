589cef06014a670d5ca52bf7cf7ee02e9238bee2
bry4n <bryann83@gmail.com>
2014-05-27 20:43:16 -0500

    Add site controller test

---
+++ spec/controllers/site_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe SiteController do
+  
+  it "GET index" do
+    get :index
+    expect(response).to render_template("index")
+  end
+
+  it "GET search" do
+    get :search, {search: "git-init"}
+    expect(assigns(:term)).to eq("git-init")
+    expect(response).to render_template("shared/search")
+  end
+
+  it "GET search_results" do
+    get :search_results, {search: "git-init"}
+    expect(assigns(:term)).to eq("git-init")
+    expect(response).to render_template("results")
+  end
+
+  it "GET svn" do
+    get :svn
+    expect(response).to render_template("svn")
+  end
+
+  it "GET redirect_wgibtx" do
+    get :redirect_wgibtx
+    expect(response).to redirect_to("http://git-scm.com/about")
+  end
+
+  describe "GET /book" do
+    
+    it "get /git-doc" do
+      request.env["PATH_INFO"] = "/git-doc"
+      get :redirect_book
+      expect(response).to redirect_to("http://git-scm.com/git-doc")
+    end
+
+    it "get /" do
+      get :redirect_book
+      expect(response).to redirect_to("http://git-scm.com/book")
+    end
+
+  end
+
+end
