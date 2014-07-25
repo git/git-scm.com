1060b5cb158195a14d0c76acd934754c6dc1a0a3
bry4n <bryann83@gmail.com>
2014-07-24 22:20:28 -0500

    Update environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
