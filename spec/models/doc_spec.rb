758e0a849e74ca48231493eee169f507415d4747
bry4n <bryann83@gmail.com>
2014-08-24 23:21:27 -0500

    Rspec is behaving werid

--- spec/models/doc_spec.rb
+++ spec/models/doc_spec.rb
@@ -2,21 +2,6 @@ require 'spec_helper'
 
 describe Doc do
 
-  # it { should have_many :doc_versions }
-
-
-  #context "AsciiDoctor" do
-    
-    let(:git_diff_file_path) { Rails.root.join("spec/data/git-diff.txt").to_s }
-    let(:git_diff_file) { File.read(git_diff_file_path) }
-
-    it "parses git-diff template", focus: true do
-      content = git_diff_file.gsub(/include::(.*)\.txt/, "include::\\1")
-      # doc = Asciidoctor::Document.new(git_diff_file) 
-      doc.source.should =~ /include::diff-options\.txt/
-      # doc.render(template_dir: Rails.root.join("templates").to_s)
-    end
-
-  #end
+  it { should have_many :doc_versions }
 
 end
