7a2ff7aed3025da5998f7811e1a924e5a00dd78a
Jason Long <jlong@github.com>
2015-03-23 17:55:31 -0400

    Gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    // Project configuration.
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compiles our Sass
+    sass: {
+      options: {
+        precision: 6,
+        sourceComments: false,
+        outputStyle: 'compressed'
+      },
+      dist: {
+        files: {
+          // 'css/gitscm.css': 'gitscm.css.scss'
+					'css/styles.css': 'sass/styles.scss'
+        }
+      }
+    },
+
+    // Handle vendor prefixing
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 8', 'ie 9']
+      },
+      dist: {
+        src: 'css/*.css'
+      },
+    },
+
+    // Runs CSS reporting
+    parker: {
+      options: {
+        metrics: [
+          'TotalStylesheets',
+          'TotalStylesheetSize',
+          'TotalRules',
+          'TotalSelectors',
+          'TotalIdentifiers',
+          'TotalDeclarations',
+          'SelectorsPerRule',
+          'IdentifiersPerSelector',
+          'SpecificityPerSelector',
+          'TopSelectorSpecificity',
+          'TopSelectorSpecificitySelector',
+          'TotalIdSelectors',
+          'TotalUniqueColours',
+          'TotalImportantKeywords',
+          'TotalMediaQueries'
+        ],
+        file: "css-stats.md",
+        usePackage: true
+      },
+      src: [
+        'css/*.css'
+      ],
+    },
+
+    // Build tooling
+
+    watch: {
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass', 'autoprefixer', 'parker']
+      }
+    },
+  });
+
+  // Load dependencies
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-build-control');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-parker');
+  grunt.loadNpmTasks('grunt-sass');
+
+  // Generate and format the CSS
+  grunt.registerTask('default', ['sass', 'autoprefixer', 'parker']);
+};
