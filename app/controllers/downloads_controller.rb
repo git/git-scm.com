3c7c06bf7b6d55a1cca3dd10582b024839b3be7b
마누엘 <nalla@hamal.uberspace.de>
2015-04-22 07:18:05 +0100

    downloads: support 32-bit/64-bit windows links
    With the switch to the *MSYS2* backend for the *Git for Windows* project
    the opportunity arose to support both `32-bit` and `64-bit` builds. In
    additon to those `32-bit` and `64-bit` builds *Git for Windows* now ships
    with portable releases as well. So it makes sense to display all those
    links on the download page. Since this behavior differs from the `mac`
    platform would be best to introduce a new download page for the `windows`
    platform.
    
    The naming convention for *Git for Windows* builds changed too, so the
    `rake downloads` task had to be adapted. It now parses the *Git for
    Windows* release file properly and also extracts the `bitness` and the
    portable information of the release file. That `bitness` and the portable
    information is then appended to the platform when storing the release
    information in the database.
    
    To provide an automatic download for the latest *Git for Windows* release
    in the correct `bitness` parsing of the user agent in the download
    controller was added. The view was updated to show the current download
    and then list the other options as well.
    
    Signed-off-by: 마누엘 <nalla@hamal.uberspace.de>

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -25,19 +25,33 @@ class DownloadsController < ApplicationController
   def download
     @platform = params[:platform]
     @platform = 'windows' if @platform == 'win'
-    if @platform == 'windows' || @platform == 'mac'
-      if @platform == 'windows'
-        @project_url  = "https://msysgit.github.io/"
-        @source_url   = "https://github.com/msysgit/git/"
-      else
-        @project_url = "http://sourceforge.net/projects/git-osx-installer/"
-        @source_url   = "https://github.com/git/git/"
-      end
+    if @platform == 'mac'
+      @project_url = "http://sourceforge.net/projects/git-osx-installer/"
+      @source_url   = "https://github.com/git/git/"
 
       @download = Download.latest_for(@platform)
       @latest = Version.latest_version
 
       render "downloads/downloading"
+    elsif @platform == 'windows'
+      @project_url = "https://git-for-windows.github.io/"
+      @source_url = "https://github.com/git-for-windows/git"
+
+      @download32 = Download.latest_for(@platform + "32")
+      @download64 = Download.latest_for(@platform + "64")
+      @download32portable = Download.latest_for(@platform + "32Portable")
+      @download64portable = Download.latest_for(@platform + "64Portable")
+      @latest = Version.latest_version
+
+      if request.env["HTTP_USER_AGENT"] =~ /WOW64|Win64/
+        @download = @download64
+        @bitness = "64-bit"
+      else
+        @download = @download32
+        @bitness = "32-bit"
+      end
+
+      render "downloads/download_windows"
     elsif @platform == 'linux'
       render "downloads/download_linux"
     else
