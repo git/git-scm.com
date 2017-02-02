2e8fa7e2a0aee3f68e190c7a20f76f8cee57b231
Adam Stankiewicz <sheerun@sher.pl>
2017-02-03 00:44:17 +0100

    Improve caching for pages and assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
