git/git-scm.com

commit 58f4e68555fc6f75d7061bffbc7472542ffe7419

Build generated command lists

lib/tasks/index.rake
@@ -59,16 +59,40 @@ task :preindex => :environment do
     doc_limit = ENV['ONLY_BUILD_DOC']
     doc_files = doc_files.select { |df| df.path =~ /#{doc_limit}/ } if doc_limit
 
-    def expand!(content, tag_files, blob_content)
+    # generate command-list content
+    categories = {}
+    if cmd_list_file = tag_files.detect { |ent| ent.path == "command-list.txt" }
+      cmd_list = blob_content[cmd_list_file.sha].match(/(### command list.*)/m)[0].split("\n").reject{|l| l =~ /^#/}.inject({}) do |list, cmd|
+        name, kind, attr = cmd.split(/\s+/)
+        list[kind] ||= []
+        list[kind] << [name, attr]
+        list
+      end
+
+      categories = cmd_list.keys.inject({}) do |list, category|
+        links = cmd_list[category].map do |cmd, attr|
+          if cmd_file = tag_files.detect { |ent| ent.path == "Documentation/#{cmd}.txt" }
+            if match = blob_content[cmd_file.sha].match(/NAME\n----\n\S+ - (.*)$/)
+              "linkgit:#{cmd}[1]::\n\t#{attr == 'deprecated' ? '(deprecated) ' : ''}#{match[1]}\n"
+            end
+          end
+        end
+        list.merge!("cmds-#{category}.txt" => links.compact.join("\n"))
+      end
+    end
+
+    def expand!(content, tag_files, blob_content, categories)
       content.gsub!(/include::(\S+)\.txt/) do |line|
         line.gsub!("include::","")
-        category_file = tag_files.detect { |ent| ent.path == "Documentation/#{line}" }
-        if category_file
-          expand!(blob_content[category_file.sha], tag_files, blob_content)
+        content_file = tag_files.detect { |ent| ent.path == "Documentation/#{line}" }
+        new_content = categories[line] || blob_content[content_file.sha]
+        if new_content
+          expand!(new_content, tag_files, blob_content, categories)
         end
       end
       return content
     end
+
     doc_files.each do |entry|
       path = File.basename( entry.path, '.txt' )
       file = DocFile.where(:name => path).first_or_create
@@ -76,7 +100,7 @@ task :preindex => :environment do
       puts "   build: #{path}"
 
       content = blob_content[entry.sha]
-      expand!(content, tag_files, blob_content)
+      expand!(content, tag_files, blob_content, categories)
       asciidoc = Asciidoctor::Document.new(content, attributes: {'sectanchors' => ''})
       asciidoc_sha = Digest::SHA1.hexdigest( asciidoc.source )
       doc = Doc.where( :blob_sha => asciidoc_sha ).first_or_create

lib/tasks/local_index.rake
@@ -52,11 +52,31 @@ task :local_index => :environment do
       puts "Found #{tree.size} entries"
       doc_limit = ENV['ONLY_BUILD_DOC']
 
-      def expand!(content, tag)
+      # generate command-list content
+      categories = {}
+      if system("git cat-file -e #{tag}:command-list.txt > /dev/null 2>&1")
+        cmd_list = `git cat-file blob #{tag}:command-list.txt`.match(/(### command list.*)/m)[0].split("\n").reject{|l| l =~ /^#/}.inject({}) do |list, cmd|
+          name, kind, attr = cmd.split(/\s+/)
+          list[kind] ||= []
+          list[kind] << [name, attr]
+          list
+        end
+
+        categories = cmd_list.keys.inject({}) do |list, category|
+          links = cmd_list[category].map do |cmd, attr|
+            if match = `git cat-file blob #{tag}:Documentation/#{cmd}.txt`.match(/NAME\n----\n\S+ - (.*)$/)
+              "linkgit:#{cmd}[1]::\n\t#{attr == 'deprecated' ? '(deprecated) ' : ''}#{match[1]}\n"
+            end
+          end
+          list.merge!("cmds-#{category}.txt" => links.compact.join("\n"))
+        end
+      end
+
+      def expand!(content, tag, categories)
         content.gsub!(/include::(\S+)\.txt/) do |line|
           line.gsub!("include::", "")
-          new_content = `git cat-file blob #{tag}:Documentation/#{line}`
-          expand!(new_content,tag)
+          new_content = categories[line] || `git cat-file blob #{tag}:Documentation/#{line}`
+          expand!(new_content, tag, categories)
         end
         return content
       end
@@ -70,7 +90,7 @@ task :local_index => :environment do
         puts "   build: #{path}"
 
         content = `git cat-file blob #{sha}`.chomp
-        expand!(content, tag)
+        expand!(content, tag, categories)
 
         asciidoc = Asciidoctor::Document.new(content, attributes: {'sectanchors' => ''})
         asciidoc_sha = Digest::SHA1.hexdigest( asciidoc.source )

commit bc3b1d8b4cd085ec8fa806245090e1df7740991d

fix script error when importing man pages of version 1.5.4 of git
58f4e68 introduced a regression where the file `command-list.txt`
started to exist but did not yet contain the header `### command
list`, resulting in a void match.

This references #639

lib/tasks/index.rake
@@ -62,7 +62,7 @@ task :preindex => :environment do
     # generate command-list content
     categories = {}
     if cmd_list_file = tag_files.detect { |ent| ent.path == "command-list.txt" }
-      cmd_list = blob_content[cmd_list_file.sha].match(/(### command list.*)/m)[0].split("\n").reject{|l| l =~ /^#/}.inject({}) do |list, cmd|
+      cmd_list = blob_content[cmd_list_file.sha].match(/(### command list.*|# command name.*)/m)[0].split("\n").reject{|l| l =~ /^#/}.inject({}) do |list, cmd|
         name, kind, attr = cmd.split(/\s+/)
         list[kind] ||= []
         list[kind] << [name, attr]

lib/tasks/local_index.rake
@@ -55,7 +55,7 @@ task :local_index => :environment do
       # generate command-list content
       categories = {}
       if system("git cat-file -e #{tag}:command-list.txt > /dev/null 2>&1")
-        cmd_list = `git cat-file blob #{tag}:command-list.txt`.match(/(### command list.*)/m)[0].split("\n").reject{|l| l =~ /^#/}.inject({}) do |list, cmd|
+        cmd_list = `git cat-file blob #{tag}:command-list.txt`.match(/(### command list.*|# command name.*)/m)[0].split("\n").reject{|l| l =~ /^#/}.inject({}) do |list, cmd|
           name, kind, attr = cmd.split(/\s+/)
           list[kind] ||= []
           list[kind] << [name, attr]