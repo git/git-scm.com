e89e6fafe044101ef3cf373db7b0bc24d9c7ae92
Jeff King <peff@peff.net>
2017-05-03 00:58:03 -0400

    rails: s/assets/files/ in config.serve_static_assets
    The former has been deprecated, and Rails produces an
    annoying warning.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
