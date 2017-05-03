4529f474441ba07a4154c41f27fdb86a920088a5
Jeff King <peff@peff.net>
2017-05-03 00:41:12 -0400

    switch rails caching from memcachier to heroku-redis
    We don't really care between the two, and this should keep
    all of our billing at Heroku (which can then be part of a
    single sponsorship).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store
+  config.cache_store = :redis_store, ENV['REDIS_URL']
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
