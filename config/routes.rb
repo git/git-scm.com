59be4cb4c228ce7cc7c4e8f74aaa70ce9829a014
chotuaashiq <akshat.maheshwari@students.iiit.ac.in>
2017-12-20 10:48:25 +0530

    Corrected the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -93,6 +93,7 @@ Gitscm::Application.routes.draw do
 
   get "/course/svn" => "site#svn"
   get "/sfc" => "site#sfc"
+  get "/trademark" => redirect("/about/trademark")
 
   get "/contributors" => redirect("https://github.com/git/git/graphs/contributors")
 
