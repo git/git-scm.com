e918a3af9b571c87bba0b72e0b28fcf89f8d11cc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-02-07 11:27:43 +0000

    overview page: add link to git-worktree
    It has been noted on
    https://public-inbox.org/git/CACsJy8Dt_TjfRk05oNW8DXrdn6s_QV8NQZKnnrgGkj3WTN_=3A@mail.gmail.com/T/#t
    that even though this is an official porcelain command there's no link
    to it from /docs, fix that.
    
    I have not run this version of the site (don't have the env to do
    that), but replicated how we're listing mergetool, so this should
    work.

--- lib/constants.rb
+++ lib/constants.rb
@@ -12,7 +12,7 @@ CMD_GROUPS = [
   ['Setup and Config', [ 'config', 'help' ]],
   ['Getting and Creating Projects', [ 'init', 'clone']],
   ['Basic Snapshotting', [ 'add', 'status', 'diff', 'commit', 'reset', 'rm', 'mv']],
-  ['Branching and Merging', [ 'branch', 'checkout', 'merge', 'mergetool', 'log', 'stash', 'tag' ]],
+  ['Branching and Merging', [ 'branch', 'checkout', 'merge', 'mergetool', 'log', 'stash', 'tag', 'worktree' ]],
   ['Sharing and Updating Projects', [ 'fetch', 'pull', 'push', 'remote', 'submodule' ]],
   ['Inspection and Comparison', [ 'show', 'log', 'diff', 'shortlog', 'describe' ]],
   ['Patching', ['am', 'apply', 'cherry-pick', 'rebase']],
