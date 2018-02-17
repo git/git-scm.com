36069f836fca98cfe87da434363287326d13a3a2
Jason Long <jlong@github.com>
2018-02-17 09:45:56 -0500

    Restructure css directory

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( git-scm.css modernizr.js modernizr-tests.js jquery-1.7.1.min.js jquery-ui-1.8.18.custom.min.js jquery.defaultvalue.js session.min.js site.js )
+Rails.application.config.assets.precompile += %w( index.css session.min.js )
