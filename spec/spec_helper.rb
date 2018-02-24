bbff798c36798a34141bb2425184b8692e6a2844
Mark Tareshawty <tarebyte@github.com>
2018-02-24 11:28:25 -0500

    Remove custom fabricator loader

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,6 @@ require 'byebug'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-Dir[Rails.root.join("spec/fabricators/**/*.rb")].each { |f| require f }
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
