94c0a18642a73acfafc5b5cb98f4e4b9eaa74a38
Mark Tareshawty <tarebyte@github.com>
2018-02-24 11:28:39 -0500

    Remove requirement of webmock since we do it in the support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 
 require 'rspec/rails'
 require 'database_cleaner'
-require 'webmock/rspec'
 require 'byebug'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
