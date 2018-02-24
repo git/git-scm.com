ba9f60ac9fd2449cc9df819fbd6eb80524f3797c
Mark Tareshawty <tarebyte@github.com>
2018-02-24 11:14:07 -0500

    Make sure version is a string before splitting it

--- app/models/version.rb
+++ app/models/version.rb
@@ -23,9 +23,9 @@ class Version < ActiveRecord::Base
   def self.version_to_num(version)
     version_int = 0.0
     mult = 1000000
-    numbers = version.split('.')
+    numbers = version.to_s.split('.')
     numbers.each do |x|
-      version_int += x.to_f * mult 
+      version_int += x.to_f * mult
       mult = mult / 100.0
     end
     version_int
