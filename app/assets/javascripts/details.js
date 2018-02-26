8cadb6e2a46d3684dc111f95104447818dda2377
Mark Tareshawty <tarebyte@github.com>
2018-02-26 11:16:33 -0500

    Remove duplicated details.js

--- app/assets/javascripts/details.js
+++
@@ -1,7 +0,0 @@
-import {on} from 'delegated-events'
-
-on('click', '.js-details-container .js-details-target', function(event) {
-  const container = this.closest('.js-details-container')
-  container.classList.toggle('open')
-  event.preventDefault()
-})
