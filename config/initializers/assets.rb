bc3d8d76f433e15ce2fe25f406c4c79afeef47e0
Mark Tareshawty <tarebyte@github.com>
2018-03-10 08:49:31 -0500

    Remove old js and remove asset.rb config for compilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile += %w( index.css session.min.js )
