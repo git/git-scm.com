30d826c2f905fdf7e98deb2c2b7e0feea14c75a3
Mark Tareshawty <tarebyte@github.com>
2018-03-11 16:13:00 -0400

    precompile modernize.js only

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-
-Rails.application.config.assets.precompile += %w( git-scm.css modernizr.js modernizr-tests.js jquery-1.7.1.min.js jquery-ui-1.8.18.custom.min.js jquery.defaultvalue.js session.min.js site.js )
+Rails.application.config.assets.precompile += %w[modernize.js]
