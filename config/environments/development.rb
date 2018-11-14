3fc462faf09279537b3740dd86a6717608bbcdb8
pedrorijo91 <pedrorijo91@gmail.com>
2018-11-14 21:22:56 +0100

    :construction: WIP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@
 
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = true
+config.webpacker.check_yarn_integrity = false
 
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,6 +3,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+config.webpacker.check_yarn_integrity = false
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
