28cb240ba1c770a35672902ae8c8c191c2556274
pedrorijo91 <pedrorijo91@gmail.com>
2019-02-10 23:18:00 +0000

    Fix download_service spec
    Test was ignored on https://github.com/git/git-scm.com/pull/1255
    
    The fix is basically mocking a DB request to return a fake Version
    when trying to save the downloads

--- spec/services/download_service_spec.rb
+++ spec/services/download_service_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe DownloadService do
 
   context "windows", :vcr do
     before do
+      mocked_version = Version.new(:id => 0, :name => "0.0.0", :commit_sha => "sha", :committed => Time.current, :created_at => Time.current, :updated_at => Time.current, :vorder => Version.version_to_num("0.0.0"))
+      allow(Version).to receive(:find_by).with(any_args).and_return(mocked_version)
       subject.download_windows_versions
     end
 
@@ -14,29 +16,31 @@ RSpec.describe DownloadService do
       expect(WebMock).to have_requested(:get, "https://api.github.com/repos/git-for-windows/git/releases")
     end
 
-    skip "creates Downloads with a windows platform" do
+    it "creates Downloads with a windows platform" do
       expect(Download.first.platform).to include("windows")
     end
 
-    skip "should have the correct download URL" do
+    it "should have the correct download URL" do
       expect(Download.first.url).to include("https://github.com/git-for-windows/git/releases/download/")
     end
   end
 
   context "mac", :vcr do
     before do
+      mocked_version = Version.new(:id => 0, :name => "0.0.0", :commit_sha => "sha", :committed => Time.current, :created_at => Time.current, :updated_at => Time.current, :vorder => Version.version_to_num("0.0.0"))
+      allow(Version).to receive(:find_by).with(any_args).and_return(mocked_version)
       subject.download_mac_versions
     end
 
-    skip "reaches out to Sourceforge for releases" do
+    it "reaches out to Sourceforge for releases" do
       expect(WebMock).to have_requested(:get, "https://sourceforge.net/projects/git-osx-installer/rss?limit=20")
     end
 
-    skip "creates Downloads with a mac platform" do
+    it "creates Downloads with a mac platform" do
       expect(Download.first.platform).to eql("mac")
     end
 
-    skip "should have the correct download URL" do
+    it "should have the correct download URL" do
       expect(Download.first.url).to include("https://sourceforge.net/projects/git-osx-installer/files/")
     end
   end
