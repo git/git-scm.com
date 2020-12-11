97d2d9a9d952dd755dfd556b78a580f075c3a6cd
Jeff King <peff@peff.net>
2020-12-11 14:39:46 -0500

    update rails files
    This is basically "rake app:update", overwriting our versions in most
    cases (because we hadn't really changed anything substantive). The big
    exception is routes.rb, which is full of app logic.

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-
 # This file is used by Rack-based servers to start the application.
 
-require ::File.expand_path("../config/environment",  __FILE__)
-run Gitscm::Application
+require_relative "config/environment"
+
+run Rails.application
+Rails.application.load_server
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-require File.expand_path("../boot", __FILE__)
+require_relative "boot"
 
 require "rails/all"
 
@@ -10,19 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Gitscm
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
