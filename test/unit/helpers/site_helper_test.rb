fe8c0d04363d7923c4ab71f3cbd5966f38cca511
Jeff King <peff@peff.net>
2022-10-05 07:05:50 -0400

    site_helper.rb: clean up unused methods
    This saves us dealing with rubocop issues in them.

--- test/unit/helpers/site_helper_test.rb
+++ test/unit/helpers/site_helper_test.rb
@@ -10,19 +10,4 @@ class SiteHelperTest < ActionView::TestCase
     assert_equal expected, src
   end
 
-  test "should create rchart" do
-    src = rchart("git", [[0, 1], [0, 1]])
-    assert_equal "<tr><td nowrap>git</td><td class='desc'></td><td class='number'> 1.00</td><td class='number'> 1.00</td><td class='number'>1x</td></tr>", src
-  end
-
-  test "should create trchart" do
-    src = trchart("git", [[0, 1], [0, 1], [0, 1]])
-    assert_equal "<tr><td nowrap>git</td><td class='desc'></td><td class='number'>  1.0</td><td class='number'>  1.0</td><td class='number'>  1.0</td></tr>", src
-  end
-
-  test "should create gchart" do
-    src = gchart("git", [[0, 1], [0, 1]])
-    assert_equal "<img src=\"https://chart.googleapis.com/chart?chxt=x&amp;cht=bvs&amp;chl=0|0&amp;chd=t:1,1&amp;chds=0,1&amp;chs=100x125&amp;chco=E09FA0|E05F49&amp;chf=bg,s,fcfcfa&chtt=git\" alt=\"init benchmarks\" />", src
-  end
-
 end
