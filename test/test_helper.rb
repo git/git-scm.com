29cd9048ea279fdbc12930f1bc9a59da056ab185
Jeff King <peff@peff.net>
2022-10-05 06:34:54 -0400

    run rubocop --autocorrect --only Layout/CommentIndentation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,13 +13,13 @@ end
 
 module ActiveSupport
   class TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
+    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+    #
+    # Note: You'll currently still have to declare fixtures explicitly in integration tests
+    # -- they do not yet inherit this setting
     fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
   end
 end
 
