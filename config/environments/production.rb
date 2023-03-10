4cdb01ba86ca05241a0424b24bdd9028fecbf8b6
Jeff King <peff@peff.net>
2023-03-10 05:12:09 -0500

    config: enable redis caching in production
    This was inadvertently lost in 97d2d9a9 (update rails files,
    2020-12-11), and I think since then we haven't been caching at all. At
    least connecting to the production redis server and running "keys *"
    shows that it has no keys (and after deploying this patch, it does).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, ENV["REDIS_URL"]
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
