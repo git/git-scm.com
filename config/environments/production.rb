79e1d52dbc1a5aaa52e72ec5fe03b4c352856d70
Jeff King <peff@peff.net>
2023-03-11 17:33:16 -0500

    use ENV.fetch to make rubocop happy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, ENV["REDIS_URL"]
+  config.cache_store = :redis_store, ENV.fetch("REDIS_URL")
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
