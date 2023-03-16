git/git-scm.com

commit 6a1d3bc891acd11e7826ab496a1902b004656687

freeze string literals in rake tasks
These were marked as not-to-be-frozen in 1d55bdbb (Don't freeze rake
tasks, 2018-11-20). But I don't see why, and now rubocop complains about
it. They seem to work fine after this change.

lib/tasks/book2.rake
@@ -1,4 +1,4 @@
-# rubocop:disable Style/FrozenStringLiteralComment
+# frozen_string_literal: true
 
 require "nokogiri"
 require "octokit"

lib/tasks/downloads.rake
@@ -1,4 +1,4 @@
-# rubocop:disable Style/FrozenStringLiteralComment
+# frozen_string_literal: true
 
 desc "find newest mac and windows binary downloads"
 task downloads: %i[windows_downloads mac_downloads]

lib/tasks/index.rake
@@ -1,4 +1,4 @@
-# rubocop:disable Style/FrozenStringLiteralComment
+# frozen_string_literal: true
 
 require "asciidoctor"
 require "octokit"

lib/tasks/search.rake
@@ -1,4 +1,4 @@
-# rubocop:disable Style/FrozenStringLiteralComment
+# frozen_string_literal: true
 
 task search_clear: :environment do
   # BONSAI.clear

commit dc247743a345ffd98e61b0a54fae56cc70cb76a6

book2: fix frozen string literal problem
Book generation has been broken since 6a1d3bc8 (freeze string literals
in rake tasks, 2022-10-05). The issue is the use of force_encoding() on
a literal, which doesn't work if it's frozen.

One solution would be to just .dup the literal. However, this
force_encoding is not necessary at all these days. Since ruby 2.0,
strings are UTF8 by default. So we can just get rid of it.

lib/tasks/book2.rake
@@ -13,7 +13,7 @@ def expand(content, path, &get_content)
     end
     new_content = get_content.call(new_fname)
     if new_content
-      expand(new_content.gsub("\xEF\xBB\xBF".force_encoding("UTF-8"), ""), new_fname) { |c| get_content.call(c) }
+      expand(new_content.gsub("\xEF\xBB\xBF", ""), new_fname) { |c| get_content.call(c) }
     else
       puts "#{new_fname} could not be resolved for expansion"
       ""