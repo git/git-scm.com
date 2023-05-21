8ba65b6b1ab13ba5afdea107c8d1753491ead7d7
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-05-21 21:12:36 +0200

    book: also generate the URLs for the downloadable formats of the book
    ... but do this only for the English version, as the other versions are
    not available in downloadable formats.
    
    As the idea is to run this script in a GitHub workflow on a shallow
    clone, let's be prepared for the situation where the tip commit of the
    default branch is _not_ (yet?) tagged and therefore no tag is present in
    the clone. In this situation, let's just go ahead and fetch all tags,
    then move along and update the URLs accordingly.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- script/update-book2.rb
+++ script/update-book2.rb
@@ -266,6 +266,17 @@ def local_genbook2(language_code, worktree_path)
     book = genbook(language_code) do |filename|
       File.open(File.join(worktree_path, filename), "r") { |infile| File.read(infile) }
     end
+    if language_code == 'en'
+      latest_tag = `git -C "#{worktree_path}" for-each-ref --format '%(refname:short)' --sort=-committerdate --count=1 refs/tags/`.chomp
+      if latest_tag.empty?
+        puts "No tag found in #{worktree_path}, trying to fetch tags"
+        latest_tag = `git -C "#{worktree_path}" fetch --tags origin && git -C "#{worktree_path}" for-each-ref --format '%(refname:short)' --sort=-committerdate --count=1 refs/tags/`.chomp
+	raise "Still no tags in #{worktree_path}?" if latest_tag.empty?
+      end
+      book.ebook_pdf = "https://github.com/progit/progit2/releases/download/#{latest_tag}/progit.pdf"
+      book.ebook_epub = "https://github.com/progit/progit2/releases/download/#{latest_tag}/progit.epub"
+      book.ebook_mobi = "https://github.com/progit/progit2/releases/download/#{latest_tag}/progit.mobi"
+    end
     book.save
   end
 end
