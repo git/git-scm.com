e510b7aa80b56ec558396a5935c1f7c5cd84d17b
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-10-15 12:00:56 +0200

    Inline the taglines on the top
    These taglines used to be stored in the Rails App, but they can easily
    live directly in `header.html`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module Gitscm
-  TAGLINES = %w[
-    fast-version-control
-    everything-is-local
-    distributed-even-if-your-workflow-isnt
-    local-branching-on-the-cheap
-    distributed-is-the-new-centralized
-  ].freeze
-
   VIDEOS = [
     [1, "41027679", "Git Basics", "What is Version Control?", "what-is-version-control", "05:59"],
     [2, "41381741", "Git Basics", "What is Git?", "what-is-git", "08:15"],
