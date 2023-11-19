6538f3d5d97e756d9b3ace4dad62bf5331b25210
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-11-19 17:33:48 +0100

    docs: correctly handle `{litdd}` in `linkgit` values
    These are expanded to `&#x2d;&#x2d;`, and we do not want those to show
    up in the `href`, but replace them by `--`.
    
    This is relevant e.g. in `git-help.txt`, where we refer to
    `linkgit:git-web{litdd}browse[1]`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- script/update-docs.rb
+++ script/update-docs.rb
@@ -116,7 +116,7 @@ def index_l10n_doc(filter_tags, doc_list, get_content)
         html = asciidoc.render
         html.gsub!(/linkgit:(\S+?)\[(\d+)\]/) do |line|
           x = /^linkgit:(\S+?)\[(\d+)\]/.match(line)
-          "<a href='/docs/#{x[1]}/#{lang}'>#{x[1]}[#{x[2]}]</a>"
+          "<a href='/docs/#{x[1].gsub(/&#x2d;/, '-')}/#{lang}'>#{x[1]}[#{x[2]}]</a>"
         end
         # HTML anchor on hdlist1 (i.e. command options)
         html.gsub!(/<dt class="hdlist1">(.*?)<\/dt>/) do |_m|
@@ -339,7 +339,8 @@ def index_doc(filter_tags, doc_list, get_content)
         html = asciidoc.render
         html.gsub!(/linkgit:(\S+?)\[(\d+)\]/) do |line|
           x = /^linkgit:(\S+?)\[(\d+)\]/.match(line)
-          "<a href='{{< relurl \"docs/#{x[1]}\" >}}'>#{x[1]}[#{x[2]}]</a>"
+          relurl = "docs/#{x[1].gsub(/&#x2d;/, '-')}"
+          "<a href='{{< relurl \"#{relurl}\" >}}'>#{x[1]}[#{x[2]}]</a>"
         end
         # HTML anchor on hdlist1 (i.e. command options)
         html.gsub!(/<dt class="hdlist1">(.*?)<\/dt>/) do |_m|
